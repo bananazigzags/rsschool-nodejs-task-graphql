04b87cac5b17200f5b9ed6f255f996c73cd6e5fd
Maria T <bananazigzags@gmail.com>
2024-03-03 22:37:28 +0300

    feat: enforce depth limit

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema, schema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -14,6 +15,8 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
+      const validationErrors = validate(schema, parse(req.body.query), [depthLimit(5)]);
+      if (validationErrors.length) return { errors: validationErrors };
       const res = await graphql({
         schema,
         source: req.body.query,
